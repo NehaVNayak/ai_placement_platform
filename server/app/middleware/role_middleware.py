cf04e1d14e962f1ace04a89bba2c0a83ff98dc38
Smital Kaginkar <kaginkarsmital@gmail.com>
2026-04-27 01:11:17 +0530

    Delete server directory

--- server/app/middleware/role_middleware.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, Depends
-from app.middleware.auth_middleware import get_current_user
-
-def require_role(required_role: str):
-    def role_checker(user=Depends(get_current_user)):
-        if user.get("role") != required_role:
-            raise HTTPException(status_code=403, detail="Access forbidden")
-        return user
-    return role_checker
--- server/app/utils/language_map.py
+++
@@ -1,5 +0,0 @@
-LANGUAGE_MAP = {
-    "python": 71,
-    "java": 62,
-    "cpp": 54
-}
